74c8d03b324888bc2b5c6aa0b151b7085784b65d
Mamus Eferha <eferhamamus@gmail.com>
2020-03-25 05:37:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+let tables = [];
+
+const filters ={
+    searchText: ''
+}
+
+$('#myInput').on('input', () =>{
+    filters.searchText = $('#myInput').val();
+    createTable(tables, filters);
+})
+
+const renderTables = function(){
+
+    db.collection('tables').get().then(data =>{
+        data.docs.forEach(element =>{
+            const singleTable = element.data();
+            tables.push(singleTable);
+        })    
+        createTable(tables, filters);
+    });
+}
+
+//METHOD TO DISPLAY TABLE
+const createTable = function(tables, filters){
+   const filteredTables = $.grep(tables, element =>{
+        return element.Patient_name.toLowerCase().includes(filters.searchText.toLowerCase());
+    })
+    $("#tbody").empty();
+    filteredTables.forEach(element =>{
+        $("#myTable").find("tBody").append("<tr><td>"+ element.Patient_name + "</td>" + "<td>" + element.Patient_age  + "</td>" + "<td>" + element.Patient_gender + "</td> </tr>");
+    })
+}
+
+$('.savePatient').click((event)=>{
+    event.preventDefault();
+    const id = uuidv4();
+    const table = {
+        Patient_name : $('#patient_name').val(),
+        Patient_age : $('#patient_age').val(),
+        Patient_gender : $('#patient_gender').val(),
+        id : id
+    }
+    db.collection('tables').doc(id).set(table).then(()=>{
+        console.log('Patient added successsfully!');
+        tables.push(table);
+        createTable(tables, filters);
+    }).catch(error=>{
+        console.log('Error adding patient', e)
+    })
+})
+
+renderTables();
---
+++ modal.js
@@ -0,0 +1,31 @@
+//ADD NEW PATIENT MODAL
+var modal = document.getElementById('modalForm');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.querySelector('.closeBtn');
+var form = document.querySelector('.formContent');
+var updateModal = document.querySelector('.updateModal');
+var closeUpdateBtn = document.querySelector('.closeUpdateBtn');
+var filterPatient = document.getElementById('filter')
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', windowClick);
+closeUpdateBtn.addEventListener('click', closeUpdateModal);
+filterPatient.addEventListener('keyup', filterItems);
+
+function openModal(){
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+function windowClick(e){
+    if(e.target == modal || e.target == updateModal){
+    modal.style.display = 'none';
+    updateModal.style.display = 'none';
+    }
+}
+function closeUpdateModal(){
+    updateModal.style.display = 'none';
+}
+
